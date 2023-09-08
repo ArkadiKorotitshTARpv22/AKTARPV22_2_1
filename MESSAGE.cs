1dc3c5ae280b0355b45a64bebb7afc24df7411c5
ArkadiKorotitshTARpv22 <arkadi.korotots@mjg.ee>
2023-09-08 10:05:44 +0300

    ulesanne

--- MESSAGE.cs
+++ MESSAGE.cs
@@ -7,88 +7,124 @@ using System.Threading.Tasks.Dataflow;
 
 namespace AKTARPV22_2_1
 {
-        public class Message
+    public class Message
+    {
+        List<string> messagelist = new List<string> { "Tere", "Hello", "Bonjour", "Morgen", "TAR","Privet" };
+        private readonly string _content;
+        private readonly string _author;
+        private readonly DateTime _time;
+        private int _likes;
+
+        public Message(string content, string author, DateTime time)
         {
-            private readonly string _content;
-            private readonly string _author;
-            private readonly DateTime _time;
-            private int _likes;
+            this._content = content;
+            this._author = author;
+            this._time = time;
+        }
+        public Message() 
+        {
 
-            public Message(string content, string author, DateTime time)
+        }
+        public int Likes { get => _likes; }
+        public DateTime Time { get => _time; }
+        public string Author { get => _author; }
+        public string Content { get => _content; }
+
+        public void AddLike()
+        {
+            _likes++;
+        }
+
+        public double GetPopularity()
+        {
+            double elapsed = DateTime.Now.Subtract(this._time).TotalSeconds;
+            if (elapsed == 0)
             {
-                this._content = content;
-                this._author = author;
-                this._time = time;
+                return _likes;
             }
+            return _likes / elapsed;
 
-            public int Likes { get => _likes; }
-            public DateTime Time { get => _time; }
-            public string Author { get => _author; }
-            public string Content { get => _content; }
-            
-            
-            public void AddLike()
-            {
-                _likes++;
-            }
+        }
 
-            public double GetPopularity()
+        public void ShowMessageInfo()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Tekst = " + Content);
+            Console.WriteLine("Aeg = " + Time);
+            Console.WriteLine("Likes = " + Likes);
+            Console.WriteLine("Populaarsus = " + GetPopularity());
+        }
+
+        public void GetPopularityInfo(List<Message> messages)
+        {
+            Message koigepopularmessage = new Message();
+            double popcounter = 0;
+            foreach (Message message in messages)
             {
-                double elapsed = DateTime.Now.Subtract(this._time).TotalSeconds;
-                if (elapsed == 0)
+                if (message.GetPopularity() > popcounter)
                 {
-                    return _likes;
+                    popcounter = message.GetPopularity();
+                    koigepopularmessage = message;
                 }
-                return _likes / elapsed;
-
             }
-        public void ShowMessage()
-        {
             
-            Console.WriteLine("Sisu: {0}\nAutor: {1}\nLikes: {2}", Content,Author,_likes);
+            Console.WriteLine("kõige populaarsem sõnum on " + koigepopularmessage.Content + " ja tema autor on " + koigepopularmessage.Author);
+
         }
 
-        public string GetPopularityInfo(double esimene, double teine)
+        public void Popularitycheck(double esimine, double teine)
         {
-            string rezult = "";
-            if (esimene > teine) { rezult = "Esimene sõnum on populaarsem kui teine"; };
-            if (esimene < teine) { rezult = "Teine sõnum on populaarsem kui esimene"; };
-            return rezult;
-        }
-
-        public string GenerateMessages(List<Message> messages)
-        {
-            Random random= new Random();
-            int RandomInt = random.Next(0, 10);
-            for (int i = 0; i == RandomInt; i++)
+            double checknum = esimine - teine;
+            if (checknum > 0)
             {
-                int a = 0;
-                a++;
-                Message m[a] = new Message("Hello", "John", DateTime.Now.AddDays(-10));
-
+                Console.WriteLine("m1 on populaarsem");
             }
+            else if (checknum < 0)
+            {
+                Console.WriteLine("m2 on vähem populaarne");
+            }
+            else
+            {
+                Console.WriteLine("m1 ja m2 on populaarsuse poolest samad");
+            }
+            return;
         }
 
-        public string GetPopularityInfoN(List<Message> messages)
+        public static void MainAuto()
         {
+            List<Message> messagelist = new List<Message> { };
 
-            string result = "";
-            double popularity = 0;
-            for (int i=0; i < messages.Count; i++)
+            Random rnd = new Random();
+            int like_counter = 0;
+            int counter = rnd.Next(2, 5);
+            Console.WriteLine("Loo {0} sõnumit", counter);
+            for (int i = 0; i < counter; i++)
             {
-                if (messages[i].GetPopularity() > popularity)
+                Console.WriteLine("Luua sõnum number {0}", i + 1);
+                string messagetext = Console.ReadLine();
+                Console.WriteLine("Mis on teie kasutajanimi");
+                string username = Console.ReadLine();
+                int t = -1 * rnd.Next(0, 1000000);
+                DateTime dhm = DateTime.Now.Date.AddSeconds(t);
+
+                Message newmessage = new Message(messagetext, username, dhm);
+                like_counter = rnd.Next(0, 50);
+                for (int ii = 0; ii < like_counter; ii++)
                 {
-                    popularity = messages[i].GetPopularity();
-                    result = messages[i].Content + " on kõige populaarsem sõuna, seda kirjutas " + messages[i].Author;
+                    newmessage.AddLike();
+
                 }
+                messagelist.Add(newmessage);
+                messagelist[i].ShowMessageInfo();
 
+                Console.WriteLine();
             }
+            messagelist[0].GetPopularityInfo(messagelist);
 
-            return result;
+
         }
-
     }
 
 
-        
+
 }
